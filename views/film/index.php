4d8da2a7fe94226262d03528ada5798654f7ef78
atetheone <atevirran@gmail.com>
2024-07-16 19:33:15 +0000

    Update film index view to use object properties instead of attributes

--- views/film/index.php
+++ views/film/index.php
@@ -13,11 +13,11 @@
   </tr>
   <?php foreach ($films as $film): ?>
   <tr>
-    <td><?php echo $film['@attributes']['id']; ?></td>
+    <td><?php echo $film->id; ?></td>
     <td><?php echo $film->titre; ?></td>
-    <td><?php echo $film->duree['@attributes']['heures'] . 'h ' . $film->duree['@attributes']['minutes'] . 'm'; ?></td>
+    <td><?php echo $film->duree_heures . 'h ' . $film->duree_minutes . 'm'; ?></td>
     <td>
-      <?php foreach ($film->genres->genre as $genre): ?>
+      <?php foreach ($film->genres as $genre): ?>
         <?php echo $genre . ' '; ?>
       <?php endforeach; ?>
     </td>
@@ -25,8 +25,8 @@
     <td><?php echo $film->langue; ?></td>
     <td><?php echo $film->annee; ?></td>
     <td>
-      <a href="index.php?controller=film&action=edit&id=<?php echo $film['@attributes']['id']; ?>">Modifier</a>
-      <a href="index.php?controller=film&action=delete&id=<?php echo $film['@attributes']['id']; ?>">Supprimer</a>
+      <a href="index.php?controller=film&action=edit&id=<?php echo $film->id; ?>">Modifier</a>
+      <a href="index.php?controller=film&action=delete&id=<?php echo $film->id; ?>">Supprimer</a>
     </td>
   </tr>
   <?php endforeach; ?>
