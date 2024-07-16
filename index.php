4d4420ba3c7d49739781b79b649c9dc41758b1db
atetheone <atevirran@gmail.com>
2024-07-16 18:14:51 +0000

    chore: Refactor index.php to use controllers for different actions

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'controllers/CinemaController.php';
+require 'controllers/RestaurantController.php';
+
+$controller = isset($_GET['controller']) ? $_GET['controller'] : 'home';
+$action = isset($_GET['action']) ? $_GET['action'] : 'index';
+
+switch ($controller) {
+  case 'film':
+    $controller = new FilmController();
+    break;
+  case 'restaurant':
+    $controller = new RestaurantController();
+    break;
+  default:
+    $controller = null;
+    break;
+}
+
+if ($controller && method_exists($controller, $action)) {
+  $controller->{$action}();
+} else {
+  echo "Page not found";
+}
