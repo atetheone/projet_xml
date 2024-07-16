9bffeeb70887723c230f219a572dc42cbd9e5d56
atetheone <atevirran@gmail.com>
2024-07-16 18:47:38 +0000

    chore: Update Film model class

--- models/Film.php
+++ models/Film.php
@@ -1,44 +1,32 @@
 <?php
 
 class Film {
-    public $titre;
-    public $dureeHeures;
-    public $dureeMinutes;
-    public $genres = [];
-    public $realisateur;
-    public $langue;
-    public $acteurs = [];
-    public $annee;
-    public $notes = [];
-    public $synopsis;
-    public $horaires = [];
+  public $id;
+  public $titre;
+  public $duree_heures;
+  public $duree_minutes;
+  public $genres = [];
+  public $realisateur;
+  public $langue;
+  public $acteurs = [];
+  public $annee;
+  public $notes = [];
+  public $synopsis;
+  public $horaires = [];
 
-    public function __construct($filmElement) {
-        $this->titre = (string) $filmElement->titre;
-        $this->dureeHeures = (string) $filmElement->durée['heures'];
-        $this->dureeMinutes = (string) $filmElement->durée['minutes'];
-        foreach ($filmElement->genres->genre as $genre) {
-            $this->genres[] = (string) $genre;
-        }
-        $this->realisateur = (string) $filmElement->réalisateur;
-        $this->langue = (string) $filmElement->langue;
-        foreach ($filmElement->acteurs->acteur as $acteur) {
-            $this->acteurs[] = (string) $acteur;
-        }
-        $this->annee = (string) $filmElement->année;
-        foreach ($filmElement->notes->note as $note) {
-            $this->notes[] = ['source' => (string) $note['source'], 'note' => (string) $note];
-        }
-        $this->synopsis = (string) $filmElement->synopsis;
-        foreach ($filmElement->horaires->horaire as $horaire) {
-            $this->horaires[] = [
-                'jour' => (string) $horaire['jour'],
-                'heure' => (string) $horaire['heure'],
-                'minute' => (string) $horaire['minute']
-            ];
-        }
-    }
-
-    
+  public function __construct($id, $titre, $duree_heures, $duree_minutes, $genres, $realisateur, $langue, $acteurs, $annee, $notes, $synopsis, $horaires) {
+    $this->id = $id;
+    $this->titre = $titre;
+    $this->duree_heures = $duree_heures;
+    $this->duree_minutes = $duree_minutes;
+    $this->genres = $genres;
+    $this->realisateur = $realisateur;
+    $this->langue = $langue;
+    $this->acteurs = $acteurs;
+    $this->annee = $annee;
+    $this->notes = $notes;
+    $this->synopsis = $synopsis;
+    $this->horaires = $horaires;
+  }
 }
-?>
+  
