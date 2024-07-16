e5ff28e11e86ae598c0ef1183885f3f137932847
atetheone <atevirran@gmail.com>
2024-07-16 19:19:06 +0000

    Define createFilmFromXML method

--- models/Cinema.php
+++ models/Cinema.php
@@ -63,6 +63,137 @@ class Cinema {
     }
   } 
 
+  // public function updateFilm($film) {
+  //   foreach (this->films->film as $filmElement) {
+  //     if ($filmElement['id'] == $film->id) {
+  //       $filmElement->titre = $film->titre;
+  //       $filmElement->durée['heures'] = $film->dureeHeures;
+  //       $filmElement->durée['minutes'] = $film->dureeMinutes;
+  //       $filmElement->genres = null;
+  //       $genres = $filmElement->addChild('genres');
+  //       foreach ($film->genres as $genre) {
+  //           $genres->addChild('genre', $genre);
+  //       }
+  //       $filmElement->réalisateur = $film->realisateur;
+  //       $filmElement->langue = $film->langue;
+  //       $filmElement->acteurs = null;
+  //       $acteurs = $filmElement->addChild('acteurs');
+  //       foreach ($film->acteurs as $acteur) {
+  //           $acteurs->addChild('acteur', $acteur);
+  //       }
+  //       $filmElement->année = $film->annee;
+  //       $filmElement->notes = null;
+  //       if (!empty($film->notes)) {
+  //           $notes = $filmElement->addChild('notes');
+  //           foreach ($film->notes as $note) {
+  //               $noteElement = $notes->addChild('note', $note['note']);
+  //               $noteElement->addAttribute('source', $note['source']);
+  //           }
+  //       }
+  //       $filmElement->synopsis = $film->synopsis;
+  //       $filmElement->horaires = null;
+  //       $horaires = $filmElement->addChild('horaires');
+  //       foreach ($film->horaires as $horaire) {
+  //         $horaireElement = $horaires->addChild('horaire');
+  //         $horaireElement->addAttribute('jour', $horaire['jour']);
+  //         $horaireElement->addAttribute('heure', $horaire['heure']);
+  //         $horaireElement->addAttribute('minute', $horaire['minute']);
+  //       }
+
+  //       $this->saveXML();
+  //       return;
+  //     }
+  //   }
+  // } 
+
+  private function loadFilmsXML() {
+    if (file_exists($this->xmlFile)) {
+      return simplexml_load_file($this->xmlFile);
+    } else {
+      throw new Exception('Échec lors de l\'ouverture du fichier ' . $this->xmlFile);
+    }
+  }
+
+  private function createFilmFromXML($film) {
+    $genres = [];
+    foreach ($film->genres->genre as $genre) {
+      $genres[] = (string) $genre;
+    }
+
+    $acteurs = [];
+    foreach ($film->acteurs->acteur as $acteur) {
+      $acteurs[] = (string) $acteur;
+    }
+
+    $notes = [];
+    foreach ($film->notes->note as $note) {
+      $notes[] = ['source' => (string) $note['source'], 'text' => (string) $note];
+    }
+
+    $horaires = [];
+    foreach ($film->horaires->horaire as $horaire) {
+      $horaires[] = [
+        'jour' => (string) $horaire['jour'],
+        'heure' => (string) $horaire['heure'],
+        'minute' => (string) $horaire['minute']
+      ];
+    }
+
+    return new Film(
+      (string) $film['id'],
+      (string) $film->titre,
+      (string) $film->duree['heures'],
+      (string) $film->duree['minutes'],
+      $genres,
+      (string) $film->realisateur,
+      (string) $film->langue,
+      $acteurs,
+      (string) $film->annee,
+      $notes,
+      (string) $film->synopsis,
+      $horaires
+    );
+  }
+
+  private function fillXMLFromFilm($filmXML, $film) {
+    $filmXML['id'] = $film->id;
+    $filmXML->titre = $film->titre;
+
+    $filmXML->duree['heures'] = $film->duree_heures;
+    $filmXML->duree['minutes'] = $film->duree_minutes;
+
+    $filmXML->genres = '';
+    foreach ($film->genres as $genre) {
+        $filmXML->genres->addChild('genre', $genre);
+    }
+
+    $filmXML->realisateur = $film->realisateur;
+    $filmXML->langue = $film->langue;
+
+    $filmXML->acteurs = '';
+    foreach ($film->acteurs as $acteur) {
+        $filmXML->acteurs->addChild('acteur', $acteur);
+    }
+
+    $filmXML->annee = $film->annee;
+
+    $filmXML->notes = '';
+    foreach ($film->notes as $note) {
+        $noteXML = $filmXML->notes->addChild('note', $note['text']);
+        $noteXML->addAttribute('source', $note['source']);
+    }
+
+    $filmXML->synopsis = $film->synopsis;
+
+    $filmXML->horaires = '';
+    foreach ($film->horaires as $horaire) {
+      $horaireXML = $filmXML->horaires->addChild('horaire');
+      $horaireXML->addAttribute('jour', $horaire['jour']);
+      $horaireXML->addAttribute('heure', $horaire['heure']);
+      $horaireXML->addAttribute('minute', $horaire['minute']);
+    }
+  }
+
   private function saveXML() {
     $this->xmlRoot->asXML($this->xmlFile);
   }
