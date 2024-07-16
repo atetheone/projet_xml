e2ba3817a7d0005af3c803537e1f767a0fdfd853
atetheone <atevirran@gmail.com>
2024-07-16 19:47:07 +0000

    feat: Add header

---
+++ views/includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portail Cinéma</title>
+  <!-- <link rel="stylesheet" href="path/to/your/css/file.css"> -->
+</head>
+<body>
+  <header>
+      <h1>Bienvenue sur le Portail Cinéma</h1>
+      <nav>
+          <ul>
+              <li><a href="index.php">Accueil</a></li>
+              <li><a href="index.php?controller=film&action=index">Films</a></li>
+              <li><a href="index.php?controller=restaurant&action=index">Restaurants</a></li>
+          </ul>
+      </nav>
+  </header>
+  <main>
