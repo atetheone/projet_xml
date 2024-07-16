c8cd581288c8b6e94e4117630b3bf43a346fb49a
atetheone <atevirran@gmail.com>
2024-07-16 19:53:18 +0000

    chore: Refactor Cinema class to use object properties instead of attributes

--- models/Cinema.php
+++ models/Cinema.php
@@ -1,28 +1,19 @@
 <?php
 
 class Cinema {
-  private $xmlFile;
   private $xmlRoot;
-  private $films = [];
 
   public function __construct($xmlFile) {
     $this->xmlFile = $xmlFile;
     $this->xmlRoot = $this->loadFilmsXML();
   }
 
-  public function getAllFilms() {
+   public function getAllFilms() {
     $films = [];
-    foreach ($this->xml->film as $film) {
+    foreach ($this->xmlRoot->film as $film) {
       $films[] = $this->createFilmFromXML($film);
     }
     return $films;
-  }
-
-  public function getFilms() {
-    foreach ($this->xmlRoot->film as $film) {
-      $this->films[] = new Film($filmElement);
-    }
-    return $this->films;
   }
 
   public function getFilmById($filmId) {
@@ -40,11 +31,9 @@ class Cinema {
     $this->saveXML();
   }
 
-  public function deleteFilm($filmId) {
-    foreach ($this->xmlRoot->film as $film) {
-      if ($film['id'] == $filmId) {
-        $dom = dom_import_simplexml($film);
-        foreach ($dom->getElementsByTagName('film') as $film) {
+  public function deleteFilm($id) {
+    $dom = dom_import_simplexml($this->xmlRoot);
+    foreach ($dom->getElementsByTagName('film') as $film) {
       if ($film->getAttribute('id') == $id) {
         $film->parentNode->removeChild($film);
         $this->saveXML();
@@ -61,50 +50,7 @@ class Cinema {
         return;
       }
     }
-  } 
-
-  // public function updateFilm($film) {
-  //   foreach (this->films->film as $filmElement) {
-  //     if ($filmElement['id'] == $film->id) {
-  //       $filmElement->titre = $film->titre;
-  //       $filmElement->durée['heures'] = $film->dureeHeures;
-  //       $filmElement->durée['minutes'] = $film->dureeMinutes;
-  //       $filmElement->genres = null;
-  //       $genres = $filmElement->addChild('genres');
-  //       foreach ($film->genres as $genre) {
-  //           $genres->addChild('genre', $genre);
-  //       }
-  //       $filmElement->réalisateur = $film->realisateur;
-  //       $filmElement->langue = $film->langue;
-  //       $filmElement->acteurs = null;
-  //       $acteurs = $filmElement->addChild('acteurs');
-  //       foreach ($film->acteurs as $acteur) {
-  //           $acteurs->addChild('acteur', $acteur);
-  //       }
-  //       $filmElement->année = $film->annee;
-  //       $filmElement->notes = null;
-  //       if (!empty($film->notes)) {
-  //           $notes = $filmElement->addChild('notes');
-  //           foreach ($film->notes as $note) {
-  //               $noteElement = $notes->addChild('note', $note['note']);
-  //               $noteElement->addAttribute('source', $note['source']);
-  //           }
-  //       }
-  //       $filmElement->synopsis = $film->synopsis;
-  //       $filmElement->horaires = null;
-  //       $horaires = $filmElement->addChild('horaires');
-  //       foreach ($film->horaires as $horaire) {
-  //         $horaireElement = $horaires->addChild('horaire');
-  //         $horaireElement->addAttribute('jour', $horaire['jour']);
-  //         $horaireElement->addAttribute('heure', $horaire['heure']);
-  //         $horaireElement->addAttribute('minute', $horaire['minute']);
-  //       }
-
-  //       $this->saveXML();
-  //       return;
-  //     }
-  //   }
-  // } 
+  }
 
   private function loadFilmsXML() {
     if (file_exists($this->xmlFile)) {
