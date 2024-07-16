0df68624f4f5e4bc82bcb7ddb97532d0069bf3d9
atetheone <atevirran@gmail.com>
2024-07-16 20:30:08 +0000

    chore: Update header.php to include CSS and font-awesome styles

--- views/includes/header.php
+++ views/includes/header.php
@@ -4,7 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Portail Cinéma</title>
-  <!-- <link rel="stylesheet" href="path/to/your/css/file.css"> -->
+  <link rel="stylesheet" href="public/css/styles.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
   <header>
