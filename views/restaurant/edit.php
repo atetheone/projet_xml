deaf6cdd0b029f0eba9df1bed837187860e7be25
atetheone <atevirran@gmail.com>
2024-07-17 19:04:11 +0000

    chore: Update restaurant edit view form layout and styling

--- views/restaurant/edit.php
+++ views/restaurant/edit.php
@@ -1,28 +1,34 @@
 <?php include 'views/includes/header.php'; ?>
 
 <h1>Modifier le Restaurant</h1>
-<form method="POST" action="index.php?controller=restaurant&action=edit&id=<?php echo $restaurant->id; ?>">
+<form method="POST" action="index.php?controller=restaurant&action=edit&id=<?= $restaurant->id; ?>">
     <label>Nom</label>
-    <input type="text" name="nom" value="<?php echo $restaurant->nom; ?>" readonly>
+    <input type="text" name="nom" value="<?= $restaurant->nom; ?>" readonly>
     
     <label>Adresse</label>
-    <input type="text" name="adresse" value="<?php echo $restaurant->adresse; ?>" required>
+    <input type="text" name="adresse" value="<?= $restaurant->adresse; ?>" required>
     
     <label>Restaurateur</label>
-    <input type="text" name="restaurateur" value="<?php echo $restaurant->restaurateur; ?>" required>
+    <input type="text" name="restaurateur" value="<?= $restaurant->restaurateur; ?>" required>
     
     <label>Description</label>
-    <textarea name="description" required><?php echo $restaurant->description; ?></textarea>
+    <textarea name="description" required>
+      <?= $restaurant->description; ?>
+    </textarea>
     
     <label>Carte (Format: id, nom, type, prix, description; Séparez les plats par des points-virgules)</label>
-    <textarea name="carte" required><?php echo implode('; ', array_map(function($plat) {
+    <textarea name="carte" required>
+      <?= implode('; ', array_map(function($plat) {
         return implode(', ', $plat);
-    }, $restaurant->carte)); ?></textarea>
+      }, $restaurant->carte)); ?>
+    </textarea>
     
     <label>Menus (Format: titre, description, prix, items séparés par |; Séparez les menus par des points-virgules)</label>
-    <textarea name="menus"><?php echo implode('; ', array_map(function($menu) {
+    <textarea name="menus">
+      <?= implode('; ', array_map(function($menu) {
         return $menu['titre'] . ', ' . $menu['description'] . ', ' . $menu['prix'] . ', ' . implode('|', $menu['items']);
-    }, $restaurant->menus)); ?></textarea>
+      }, $restaurant->menus)); ?>
+    </textarea>
     
     <input type="submit" value="Modifier le Restaurant">
 </form>
