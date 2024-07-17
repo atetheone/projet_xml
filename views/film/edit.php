a051be527b07a2d49309f6674d94ed1cb52526cd
atetheone <atevirran@gmail.com>
2024-07-17 19:03:49 +0000

    Update film edit view form layout and styling

--- views/film/edit.php
+++ views/film/edit.php
@@ -1,41 +1,41 @@
 <?php include 'views/includes/header.php'; ?>
 
 <h1>Modifier le Film</h1>
-<form method="POST" action="index.php?controller=film&action=edit&id=<?php echo $film->id; ?>">
+<form method="POST" action="index.php?controller=film&action=edit&id=<?= $film->id; ?>">
     <label>Titre</label>
-    <input type="text" name="titre" value="<?php echo $film->titre; ?>" required>
+    <input type="text" name="titre" value="<?= $film->titre; ?>" required>
     
     <label>Durée (heures)</label>
-    <input type="number" name="duree_heures" value="<?php echo $film->duree_heures; ?>" required>
+    <input type="number" name="duree_heures" value="<?= $film->duree_heures; ?>" required>
     
     <label>Durée (minutes)</label>
-    <input type="number" name="duree_minutes" value="<?php echo $film->duree_minutes; ?>" required>
+    <input type="number" name="duree_minutes" value="<?= $film->duree_minutes; ?>" required>
     
     <label>Genres</label>
-    <input type="text" name="genres" value="<?php echo implode(', ', $film->genres); ?>" required>
+    <input type="text" name="genres" value="<?= implode(', ', $film->genres); ?>" required>
     
     <label>Réalisateur</label>
-    <input type="text" name="realisateur" value="<?php echo $film->realisateur; ?>" required>
+    <input type="text" name="realisateur" value="<?= $film->realisateur; ?>" required>
     
     <label>Langue</label>
-    <input type="text" name="langue" value="<?php echo $film->langue; ?>" required>
+    <input type="text" name="langue" value="<?= $film->langue; ?>" required>
     
     <label>Année</label>
-    <input type="number" name="annee" value="<?php echo $film->annee; ?>" required>
+    <input type="number" name="annee" value="<?= $film->annee; ?>" required>
     
     <label>Synopsis</label>
-    <textarea name="synopsis" required><?php echo $film->synopsis; ?></textarea>
+    <textarea name="synopsis" required><?= $film->synopsis; ?></textarea>
     
     <label>Acteurs</label>
-    <input type="text" name="acteurs" value="<?php echo implode(', ', $film->acteurs); ?>" required>
+    <input type="text" name="acteurs" value="<?= implode(', ', $film->acteurs); ?>" required>
     
     <label>Notes</label>
-    <textarea name="notes"><?php echo implode('; ', array_map(function($note) {
+    <textarea name="notes"><?= implode('; ', array_map(function($note) {
         return $note['source'] . ':' . $note['text'];
     }, $film->notes)); ?></textarea>
     
     <label>Horaires</label>
-    <textarea name="horaires"><?php echo implode('; ', array_map(function($horaire) {
+    <textarea name="horaires"><?= implode('; ', array_map(function($horaire) {
         return $horaire['jour'] . ':' . $horaire['heure'] . ':' . $horaire['minute'];
     }, $film->horaires)); ?></textarea>
     
