18e2074ad4f3b9a3e28aa4e9b223d02f32693bba
atetheone <atevirran@gmail.com>
2024-07-17 20:50:23 +0000

    feat: Add authentication functionality to controllers and views

--- index.php
+++ index.php
@@ -1,25 +1,67 @@
 <?php
+session_start();
+require_once 'controllers/AuthController.php';
+require_once 'controllers/HomeController.php';
+require_once 'controllers/CinemaController.php';
+require_once 'controllers/RestaurantController.php';
 
-require 'controllers/CinemaController.php';
-require 'controllers/RestaurantController.php';
-
+// Définir le contrôleur et l'action par défaut
 $controller = isset($_GET['controller']) ? $_GET['controller'] : 'home';
 $action = isset($_GET['action']) ? $_GET['action'] : 'index';
 
+// Router les requêtes vers les contrôleurs appropriés
 switch ($controller) {
-  case 'film':
-    $controller = new CinemaController();
+  case 'auth':
+    $authController = new AuthController();
+    if ($action === 'login') {
+      $authController->login();
+    } elseif ($action === 'logout') {
+      $authController->logout();
+    } else {
+      echo "Action non trouvée";
+    }
     break;
-  case 'restaurant':
-    $controller = new RestaurantController();
-    break;
-  default:
-    $controller = null;
-    break;
-}
 
-if ($controller && method_exists($controller, $action)) {
-  $controller->{$action}();
-} else {
-  echo "Page not found";
+  case 'home':
+    $homeController = new HomeController();
+    if ($action === 'index') {
+      $homeController->index();
+    } else {
+      echo "Action non trouvée";
+    }
+    break;
+  
+  case 'film':
+    $cinemaController = new CinemaController();
+    if ($action === 'index') {
+      $cinemaController->index();
+    } elseif ($action === 'add') {
+      $cinemaController->add();
+    } elseif ($action === 'edit') {
+      $cinemaController->edit();
+    } elseif ($action === 'delete') {
+      $cinemaController->delete();
+    } else {
+      echo "Action non trouvée";
+    }
+    break;
+  
+  case 'restaurant':
+    $restaurantController = new RestaurantController();
+    if ($action === 'index') {
+      $restaurantController->index();
+    } elseif ($action === 'add') {
+      $restaurantController->add();
+    } elseif ($action === 'edit') {
+      $restaurantController->edit();
+    } elseif ($action === 'delete') {
+      $restaurantController->delete();
+    } else {
+      echo "Action non trouvée";
+    }
+    break;
+
+  default:
+    echo "Contrôleur non trouvé";
+    break;
 }
