7f86f5496e9216aed7fadbdb73eb40ef0e12a658
Maguette NDIAYE <maguettendiaye1@esp.sn>
2024-07-19 16:26:20 +0000

    affichage des horaires

--- views/film/show.php
+++ views/film/show.php
@@ -1,6 +1,5 @@
 <?php include 'views/includes/header.php'; ?>
 
-
 <h1 class="title">Détails du Film</h1>
 <h2><?php echo $film->titre; ?></h2>
 <p><strong>Durée:</strong> <?= $film->duree_heures . 'h ' . $film->duree_minutes . 'mn'; ?></p>
@@ -10,13 +9,39 @@
 <p><strong>Acteurs:</strong> <?= implode(', ', $film->acteurs); ?></p>
 <p><strong>Année:</strong> <?= $film->annee; ?></p>
 <p><strong>Synopsis:</strong> <?= $film->synopsis; ?></p>
-<p>
-  <strong>Horaires:</strong> 
-  <?= implode('; ', array_map(function($horaire) {
-        return $horaire['jour'] . ':' . $horaire['heure'] . ':' . $horaire['minute'];
-  }, $film->horaires)); ?>
-</p>
+
+<h3>Horaires</h3>
+
+<?php
+// Organiser les horaires par jour
+$horairesParJour = [];
+foreach ($film->horaires as $horaire) {
+    $jour = $horaire['jour'];
+    $heure = $horaire['heure'] . ':' . $horaire['minute'];
+    if (!isset($horairesParJour[$jour])) {
+        $horairesParJour[$jour] = [];
+    }
+    $horairesParJour[$jour][] = $heure;
+}
+?>
+
+<table>
+    <thead>
+        <tr>
+            <th>Jour</th>
+            <th>Heures</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($horairesParJour as $jour => $heures): ?>
+            <tr>
+                <td><?= $jour; ?></td>
+                <td><?= implode(' | ', $heures); ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
 <a href="index.php?controller=film&action=index" class="btn">Retour à la liste</a>
 
-    
 <?php include 'views/includes/footer.php'; ?>
