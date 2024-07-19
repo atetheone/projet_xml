2d7fab5e46ccce1b281debf93d6aa3f2275b17a5
atetheone <atevirran@gmail.com>
2024-07-19 23:12:13 +0000

    chore: Add required labels to film form fields

--- views/film/add.php
+++ views/film/add.php
@@ -2,38 +2,39 @@
 
 <h1 class="title">Ajouter un Film</h1>
 <form class="add-form" method="POST" action="index.php?controller=film&action=add">
-  <label>Titre</label>
+  <label>Titre *</label>
   <input type="text" name="titre" required>
 
-  <label>Durée (heures)</label>
-  <input type="number" name="duree_heures" required>
+  <label>Durée (heures) *</label>
+  <input type="number" name="duree_heures" min="0" max="23" required>
+
+  <label>Durée (minutes) *</label>
+  <input type="number" name="duree_minutes" min="0" max="59" step="1" required>
+
   
-  <label>Durée (minutes)</label>
-  <input type="number" name="duree_minutes" required>
-  
-  <label>Genres</label>
+  <label>Genres *</label>
   <input type="text" name="genres" placeholder="Séparez par des virgules" required>
   
-  <label>Réalisateur</label>
+  <label>Réalisateur *</label>
   <input type="text" name="realisateur" required>
   
-  <label>Langue</label>
+  <label>Langue *</label>
   <input type="text" name="langue" required>
   
-  <label>Année</label>
+  <label>Année *</label>
   <input type="number" name="annee" required>
   
-  <label>Synopsis</label>
+  <label>Synopsis *</label>
   <textarea name="synopsis" required></textarea>
   
-  <label>Acteurs</label>
+  <label>Acteurs *</label>
   <input type="text" name="acteurs" placeholder="Séparez par des virgules" required>
   
-  <label>Notes</label>
-  <textarea name="notes" placeholder="Format: source:note; Séparez par des virgules"></textarea>
+  <label>Notes (sur 5)</label>
+  <textarea name="notes" placeholder="Format: source:note; Séparez par des points virgules"></textarea>
   
   <label>Horaires</label>
-  <textarea name="horaires" placeholder="Format: jour:heure:minute; Séparez par des virgules"></textarea>
+  <textarea name="horaires" placeholder="Format: jour:heure:minute; Séparez par des points virgules"></textarea>
   
   <input type="submit" value="Ajouter le Film">
 </form>
